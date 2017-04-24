dd28434fe46049525465baa2d548f6d48c46fd63
Muhammad Waqas <waqasazamkml@gmail.com>
2017-04-24 18:19:49 +0500

    Initial commit

---
+++ app/src/main/java/com/dozydroid/splashlayoutinflater/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.dozydroid.splashlayoutinflater;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                SplashActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
