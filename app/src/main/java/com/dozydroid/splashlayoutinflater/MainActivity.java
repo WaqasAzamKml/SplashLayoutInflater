f4376bc6c622c68bffdf05b15d6471014cc762fa
Muhammad Waqas <waqasazamkml@gmail.com>
2017-04-25 17:47:46 +0500

    Implemented layoutInflater

--- app/src/main/java/com/dozydroid/splashlayoutinflater/MainActivity.java
+++ app/src/main/java/com/dozydroid/splashlayoutinflater/MainActivity.java
@@ -2,12 +2,25 @@ package com.dozydroid.splashlayoutinflater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LayoutInflater layoutInflater;
+    View btnView;
+    RelativeLayout layoutContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutContainer = (RelativeLayout) findViewById(R.id.layoutContainer);
+
+        layoutInflater = getLayoutInflater();
+        btnView = layoutInflater.inflate(R.layout.button_layout, null);
+
+        layoutContainer.addView(btnView);
     }
 }
